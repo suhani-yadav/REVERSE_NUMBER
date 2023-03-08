56b54d3e7a45b6fba93e7b1ec948780d08a8d7bc
suhani-yadav <96808266+suhani-yadav@users.noreply.github.com>
2023-03-08 20:40:57 +0530

    Update REVERSE.py

--- REVERSE.py
+++ REVERSE.py
@@ -1,1 +1,12 @@
 print("reverse a number")
+# take inputs
+num = int(input('Enter an integer number: '))
+# calculate reverse of number
+reverse = 0
+while(num > 0):
+    last_digit = num % 10
+    reverse = reverse * 10 + last_digit
+    num = num // 10
+
+# display result
+print('The reverse number is = ', reverse)
